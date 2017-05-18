e3948bebb016818afac08ebab467efee95a17fb8
Russell Gordon <rgordon@rsgc.on.ca>
2017-05-18 13:34:39 -0400

    Initial Commit

---
+++ ExamPrepCalendar/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ExamPrepCalendar
+//
+//  Created by Russell Gordon on 5/18/17.
+//  Copyright © 2017 Russell Gordon. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
